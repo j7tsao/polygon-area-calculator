f2df54dc0c2a6d06fd5c5c498f535b634154944c
j7tsao <j7tsao@gmail.com>
2022-02-09 11:57:17 -0800

    add shebang

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,84 +1,92 @@
-class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-    
-    def __str__(self):
-        return f"Rectangle(width={self.width}, height={self.height})"
-    
-    def set_width(self, width):
-        self.width = width
-    
-    def set_height(self, height):
-        self.height = height
-    
-    def get_area(self):
-        return self.width * self.height
-    
-    def get_perimeter(self):
-        return 2 * ( self.width + self.height )
+#!/usr/bin/python3.9
 
-    def get_diagonal(self):
-        return (self.width ** 2 + self.height ** 2) ** .5 
-
-    def get_picture(self):
+def main():
         
-        if self.width > 50 or self.height > 50:
-            return "Too big for picture."
-        else:
-            display = str()
-            star = "*"
-            nl = '\n'
-            num = self.height
-            while num > 0:
-                display += star * self.width + nl
-                num -= 1
-            # or method 2:
-            # display = ((star * self.width) + nl) * self.height
-            return display
+    class Rectangle:
+        def __init__(self, width, height):
+            self.width = width
+            self.height = height
+        
+        def __str__(self):
+            return f"Rectangle(width={self.width}, height={self.height})"
+        
+        def set_width(self, width):
+            self.width = width
+        
+        def set_height(self, height):
+            self.height = height
+        
+        def get_area(self):
+            return self.width * self.height
+        
+        def get_perimeter(self):
+            return 2 * ( self.width + self.height )
 
-    def get_amount_inside(self, obj):
-        if self.width > obj.width and self.height > obj.height:
-            return self.get_area() // obj.get_area()
-        else:
-            return 0   
+        def get_diagonal(self):
+            return (self.width ** 2 + self.height ** 2) ** .5 
 
-class Square(Rectangle):
-    # def __init__(self, side):
-    #     super().__init__(side, side)
+        def get_picture(self):
+            
+            if self.width > 50 or self.height > 50:
+                return "Too big for picture."
+            else:
+                display = str()
+                star = "*"
+                nl = '\n'
+                num = self.height
+                while num > 0:
+                    display += star * self.width + nl
+                    num -= 1
+                # or method 2:
+                # display = ((star * self.width) + nl) * self.height
+                return display
 
-    def __init__(self, side):
-        self.width = side
-        self.height = side
+        def get_amount_inside(self, obj):
+            if self.width > obj.width and self.height > obj.height:
+                return self.get_area() // obj.get_area()
+            else:
+                return 0   
 
-    def __str__(self):
-        return f"Square(side={self.width})"
+    class Square(Rectangle):
+        # def __init__(self, side):
+        #     super().__init__(side, side)
 
-    def set_side(self, side):
-        self.width = side
-        self.height = side
+        def __init__(self, side):
+            self.width = side
+            self.height = side
 
-# instantiation & testing
+        def __str__(self):
+            return f"Square(side={self.width})"
 
-rect = Rectangle(10, 5)
-print(rect.get_area())
-rect.set_height(3)
-print(rect.get_perimeter())
-print(rect)
-print(rect.get_picture())
+        def set_side(self, side):
+            self.width = side
+            self.height = side
 
-sq = Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
-print(sq.get_picture())
 
-rect.set_height(8)
-rect.set_width(16)
-print(rect.get_amount_inside(sq))
+    # instantiation & testing
+    rect = Rectangle(10, 5)
+    print(rect.get_area())
+    rect.set_height(3)
+    print(rect.get_perimeter())
+    print(rect)
+    print(rect.get_picture())
 
-sq.set_side(2)
-print(sq)
-sq.set_width(4)
-print(sq)
+    sq = Square(9)
+    print(sq.get_area())
+    sq.set_side(4)
+    print(sq.get_diagonal())
+    print(sq)
+    print(sq.get_picture())
+
+    rect.set_height(8)
+    rect.set_width(16)
+    print(rect.get_amount_inside(sq))
+
+    sq.set_side(2)
+    print(sq)
+    sq.set_width(4)
+    print(sq)
+
+
+if __name__ == '__main__':
+    main()
